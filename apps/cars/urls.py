7af6fa41086e7a8ba0f05458578a100de02e11a1
Ostap Yurchuk <ostap4uk43yurchuk@gmail.com>
2023-03-29 22:58:55 +0300

    lesson4

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -4,5 +4,5 @@ from .views import CarsGetPost, CarRetrieveUpdateDelete
 
 urlpatterns = [
     path('', CarsGetPost.as_view(), name='cars_get_post'),
-    path('/<int:pk>', CarRetrieveUpdateDelete.as_view(), name='cars_get_update_delete')
+    path('/<int>', CarRetrieveUpdateDelete.as_view(), name='cars_get_update_delete')
 ]
